d110e8ae2eff0d81594ff82adac4e5e7b5ab163c
Mateus Guedelho Serejo <mateus.guedelho@moringadigital.com.br>
2018-08-28 01:30:18 -0300

    new fields and controllers

--- src/containers/home/Home.js
+++ src/containers/home/Home.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {connect} from 'react-redux';
 
 import './Home.css';
@@ -7,12 +7,17 @@ import Header from '../../components/header/Header';
 import Main from '../../components/main/Main';
 import Footer from '../../components/footer/Footer';
 
-import {updatePlayerName, updatePlayerTurn, updateMatrix} from "../../modules/tictactoe";
+import {
+  updatePlayerName,
+  updatePlayerTurn,
+  updatePlayerVictory,
+  updateGameCount,
+  updateMatrix
+} from "../../modules/tictactoe";
 
 class Home extends Component {
-
   onClickBlock = (e, i, j) => {
-    if(!this.props.matrix[i][j]) {
+    if (!this.props.matrix[i][j]) {
       let matrix = this.props.matrix;
       let playerTurn = this.props.playerTurn;
 
@@ -21,33 +26,62 @@ class Home extends Component {
       matrix[i][j] = playerTurn;
       this.props.updateMatrix(matrix);
 
-      this.gameController(matrix, playerTurn);
+      this.gameController(matrix);
 
-      playerTurn = playerTurn === 1 ? 2:1;
+      playerTurn = playerTurn === 1 ? 2 : 1;
       this.props.updatePlayerTurn(playerTurn);
     } else {
       console.log("não pode");
     }
   };
 
-  gameController = (matrix, playerTurn) => {
+  gameController = (matrix) => {
     let result = this.verifyLine(matrix, false, false) || this.verifyLine(this.reverseMatrix(matrix), true, false) || this.verifyLine(this.diagonalMatrix(matrix), false, true);
-    
-    if(result){
-      if(result.winner === 1){
+
+    if (result) {
+      let winnerLine = document.querySelector(".winner-line");
+
+      if (result.winner === 1) {
         console.log(this.props.playerName1);
+        let playerVictory1 = this.props.playerVictory1;
+        playerVictory1++;
+        this.props.updatePlayerVictory("player1", playerVictory1);
       } else {
         console.log(this.props.playerName2);
+        let playerVictory2 = this.props.playerVictory2;
+        playerVictory2++;
+        this.props.updatePlayerVictory("player1", playerVictory2);
       }
-      console.log(result);
+
+      if (result.isReverse) {
+        winnerLine.classList.add("reverse");
+      } else if (result.isDiagonal) {
+        winnerLine.classList.add("diagonal");
+      } else {
+        winnerLine.classList.add("normal");
+      }
+      winnerLine.classList.add("index" + result.lineIndex);
     } else if (this.isFull(matrix)) {
       console.log("empate");
     }
+
+    if(result || this.isFull(matrix)){
+      let gameCount = this.props.gameCount;
+      gameCount++;
+      this.props.updateGameCount(gameCount);
+      this.props.updateMatrix(null);
+    }
+  };
+
+  resetGameInterface = () => {
+    let winnerLine = document.querySelector(".winner-line");
+    winnerLine.className = "winner-line";
+    [...document.querySelectorAll(".value")].map(element => element.className = "value");
   };
 
   verifyLine = (matrix, isReverse, isDiagonal) => {
-    for(let i = 0; i < matrix.length; i++){
-      if(matrix[i].every((value) => value === 1) || matrix[i].every((value) => value === 2)){
+    for (let i = 0; i < matrix.length; i++) {
+      if (matrix[i].every((value) => value === 1) || matrix[i].every((value) => value === 2)) {
         return {
           winner: matrix[i][0],
           lineIndex: i,
@@ -56,44 +90,59 @@ class Home extends Component {
         };
       }
     }
-  }
+  };
 
   diagonalMatrix = (matrix) => {
     let diagonalX = [];
     let diagonalY = [];
 
-    for(let i = 0; i < matrix.length; i++) {
+    for (let i = 0; i < matrix.length; i++) {
       diagonalX.push(matrix[i][i]);
       diagonalY.push(matrix[i][matrix.length - i - 1]);
     }
 
     return [diagonalX, diagonalY];
-  }
+  };
 
   reverseMatrix = (matrix) => matrix.map((line, i) => line.map((value, j) => matrix[j][i]));
 
   isFull = (matrix) => matrix.map(line => line.every(value => value !== 0)).every(value => value === true);
 
   render() {
-    const {playerName1, playerName2, updatePlayerName, matrix} = this.props;
+    const {
+      playerName1,
+      playerVictory1,
+      playerName2,
+      playerVictory2,
+      updatePlayerName,
+      playerTurn,
+      gameCount,
+      matrix
+    } = this.props;
     return (
       <div className="Home">
-        <Header />
+        <Header/>
         <Main>
-          {/* <h1>TicTacToe</h1> */}
           {/* <input name="player1" value={playerName1} onChange={updatePlayerName}/>  */}
           {/* <input name="player2" value={playerName2} onChange={updatePlayerName}/> */}
           <div className="container">
-            <div className="matrix">
-              {matrix.map((line, i) => (
-                line.map((value, j) => (
-                  <div key={i + "" + j} className="value" onClick={(e) => this.onClickBlock(e, i, j)}></div>
-                ))
-              ))}
+            <div className="content">
+              <div className="winner-line">
+              </div>
+              <div className="matrix">
+                {matrix.map((line, i) => (
+                  line.map((value, j) => (
+                    <div key={i + "" + j}
+                         className="value"
+                         onClick={(e) => this.onClickBlock(e, i, j)}>
+                    </div>
+                  ))
+                ))}
+              </div>
             </div>
           </div>
         </Main>
-        <Footer />
+        <Footer/>
       </div>
     );
   }
@@ -101,14 +150,19 @@ class Home extends Component {
 
 const initMapStateToProps = state => ({
   playerName1: state.tictactoe.playerName1,
+  playerVictory1: state.tictactoe.playerVictory1,
   playerName2: state.tictactoe.playerName2,
+  playerVictory2: state.tictactoe.playerVictory2,
   playerTurn: state.tictactoe.playerTurn,
+  gameCount: state.tictactoe.gameCount,
   matrix: state.tictactoe.matrix
 });
 
 const initMapDispatchToProps = dispatch => ({
   updatePlayerName: (e) => dispatch(updatePlayerName(e.target)),
   updatePlayerTurn: (playerTurn) => dispatch(updatePlayerTurn(playerTurn)),
+  updateGameCount: (gameCount) => dispatch(updateGameCount(gameCount)),
+  updatePlayerVictory: (player, victory) => dispatch(updatePlayerVictory(player, victory)),
   updateMatrix: (matrix) => dispatch(updateMatrix(matrix))
 });
 
--- src/modules/tictactoe.js
+++ src/modules/tictactoe.js
@@ -1,12 +1,18 @@
 const CHANGE_PLAYER_NAME_1 = 'CHANGE_PLAYER_NAME_1';
+const CHANGE_PLAYER_VICTORY_1 = 'CHANGE_PLAYER_VICTORY_1';
 const CHANGE_PLAYER_NAME_2 = 'CHANGE_PLAYER_NAME_2';
+const CHANGE_PLAYER_VICTORY_2 = 'CHANGE_PLAYER_VICTORY_2';
 const CHANGE_PLAYER_TURN = 'CHANGE_PLAYER_TURN';
+const CHANGE_GAME_COUNT = 'CHANGE_GAME_COUNT';
 const CHANGE_MATRIX = 'CHANGE_MATRIX';
 
 const initialState = {
   playerName1: '',
+  playerVictory1: 0,
   playerName2: '',
+  playerVictory2: 0,
   playerTurn: 1,
+  gameCount: 0,
   matrix: [[0,0,0]
           ,[0,0,0]
           ,[0,0,0]]
@@ -19,10 +25,25 @@ export default (state = initialState, action = {}) => {
         ...state,
         playerName1: action.payload
       };
+    case CHANGE_PLAYER_VICTORY_1:
+      return {
+        ...state,
+        playerVictory1: action.payload
+      };
     case CHANGE_PLAYER_NAME_2:
       return {
         ...state,
         playerName2: action.payload
+      };
+    case CHANGE_PLAYER_VICTORY_2:
+      return {
+        ...state,
+        playerVictory2: action.payload
+      };
+    case CHANGE_GAME_COUNT:
+      return {
+        ...state,
+        gameCount: action.payload
       };
     case CHANGE_PLAYER_TURN:
       return {
@@ -44,6 +65,16 @@ export const updatePlayerName = target => ({
   payload: target.value
 });
 
+export const updatePlayerVictory = (player, victory) => ({
+  type: player === "player1" ? CHANGE_PLAYER_VICTORY_1:CHANGE_PLAYER_VICTORY_2,
+  payload: victory
+});
+
+export const updateGameCount = gameCount => ({
+  type: CHANGE_GAME_COUNT,
+  payload: gameCount
+});
+
 export const updatePlayerTurn = playerTurn => ({
   type: CHANGE_PLAYER_TURN,
   payload: playerTurn
@@ -51,5 +82,5 @@ export const updatePlayerTurn = playerTurn => ({
 
 export const updateMatrix = matrix => ({
   type: CHANGE_MATRIX,
-  payload: matrix
+  payload: matrix || [[0,0,0], [0,0,0],[0,0,0]]
 });
